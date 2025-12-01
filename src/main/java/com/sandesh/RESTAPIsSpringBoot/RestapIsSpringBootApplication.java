b41a2dd180eabc1f09cffc6ae4207b9d7f832c42
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2025-12-01 16:38:09 +0545

    setup everything for spring boot rest

---
+++ src/main/java/com/sandesh/RESTAPIsSpringBoot/RestapIsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sandesh.RESTAPIsSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapIsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapIsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandesh/RESTAPIsSpringBoot/RestapIsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandesh.RESTAPIsSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapIsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
