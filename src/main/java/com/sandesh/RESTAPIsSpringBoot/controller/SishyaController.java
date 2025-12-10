36a2359b67d0df5acec9fc671a29b468f9bc4e39
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2025-12-10 20:05:43 +0545

    GET APIs using Repository Interface

--- src/main/java/com/sandesh/RESTAPIsSpringBoot/controller/SishyaController.java
+++ src/main/java/com/sandesh/RESTAPIsSpringBoot/controller/SishyaController.java
@@ -1,14 +1,24 @@
 package com.sandesh.RESTAPIsSpringBoot.controller;
 
 import com.sandesh.RESTAPIsSpringBoot.dto.SishyaDto;
+import com.sandesh.RESTAPIsSpringBoot.entity.Sishya;
+import com.sandesh.RESTAPIsSpringBoot.repository.SishyaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SishyaController {
 
-    @GetMapping("/sishya")
-    public SishyaDto getSishya() {
-        return new SishyaDto(1L, "Sandesh", "Sindhuli","Khatiwadasandesh@gmail.com");
+    private final SishyaRepository sishyaRepository;
+
+    public SishyaController(SishyaRepository sishyaRepository) {
+        this.sishyaRepository = sishyaRepository;
+    }
+
+    @GetMapping("/sishyaViewAll")
+    public List<Sishya> getSishya() {
+        return sishyaRepository.findAll();
     }
 }
--- src/main/java/com/sandesh/RESTAPIsSpringBoot/repository/SishyaRepository.java
+++ src/main/java/com/sandesh/RESTAPIsSpringBoot/repository/SishyaRepository.java
@@ -1,4 +1,9 @@
 package com.sandesh.RESTAPIsSpringBoot.repository;
 
-public class SishyaRepository {
+import com.sandesh.RESTAPIsSpringBoot.entity.Sishya;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SishyaRepository extends JpaRepository<Sishya, Long> {
 }
